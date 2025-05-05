99bad5bf4503cf25e974e6e87cadfc204c4b4418
Alex Yersh <yersh.alex@gmail.com>
2025-05-05 12:47:01 +0300

    Вызов клиента

--- main.go
+++ main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"fmt"
+	"github.com/AlexLuminare/read_advisor_bot/clients/telegram"
 	"github.com/joho/godotenv"
 	"os"
 )
 
 func main() {
-	godotenv.Load(".env")
+	err := godotenv.Load(".env")
+	if err != nil {
+		return
+	}
 	token := os.Getenv("TG_TOKEN")
-
+	host := os.Getenv("HOST")
 	fmt.Printf("token: %s", token)
+
+	tgClient = telegram.New(host, token)
+
+	//fetcher =fetcher.New()
+
+	//processor =processor.New()
+
+	//consumer.Start(fetcher, processor)
+
 }
